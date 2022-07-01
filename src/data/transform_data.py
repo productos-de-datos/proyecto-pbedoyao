41c15922cc8af520e52a11f73dbe102d2bcb5d94
pbedoyao <101910326+pbedoyao@users.noreply.github.com>
2022-06-30 23:31:09 -0500

    ok

--- src/data/transform_data.py
+++ src/data/transform_data.py
@@ -20,28 +20,20 @@ def transform_data():
     
     
     for Archivos in ArchivosLanding:
-        #try:            
-            dfArchivo = pd.read_excel(Archivos)
+        try:            
+            dfArchivo = pd.read_excel("data_lake/landing/" + Archivos)
             if dfArchivo.columns[0] != "Fecha":
-
                 FilaInicianDatos = dfArchivo[dfArchivo.iloc[:, 0] == "Fecha"].index[0] + 1
                 dfArchivo = pd.read_excel(Archivos, skiprows=FilaInicianDatos)
                 NombreArchivo = Archivos.split("/")[-1]
                 NombreArchivoCSV = NombreArchivo.split(".")[0] + ".csv"
-                #ArchivoLocal = open(f"data_lake/landing/" + NombreArchivoCSV, "wb")
-                #ArchivoLocal.write(dfArchivo)
-                #ArchivoLocal.close()
-                dfArchivo.to_csv("./data_lake/raw/" + NombreArchivoCSV, index=False)
-                
+                dfArchivo.to_csv("./data_lake/raw/" + NombreArchivoCSV, index=False) 
             else:
-
                 NombreArchivo = Archivos.split("/")[-1]
                 NombreArchivoCSV = NombreArchivo.split(".")[0] + ".csv"
-                dfArchivo.to_csv("./data_lake/raw/" + NombreArchivoCSV, index=False)
-                
-        #except:
-            
-        #    print("Error al transformar el archivo " + Archivos )
+                dfArchivo.to_csv("./data_lake/raw/" + NombreArchivoCSV, index=False) 
+        except:
+            print("Error al transformar el archivo " + Archivos )
         
     
     #raise NotImplementedError("Implementar esta función")
