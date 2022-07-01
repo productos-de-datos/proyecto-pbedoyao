25f4e0f73c25c19c3a113fab63be1c5baf9d5129
pbedoyao <101910326+pbedoyao@users.noreply.github.com>
2022-07-01 00:47:33 -0500

    ok

--- src/data/clean_data.py
+++ src/data/clean_data.py
@@ -37,7 +37,7 @@ def clean_data():
 
         Data.columns = ["Fecha","H00","H01","H02","H03","H04","H05","H06","H07","H08","H09","H10","H11","H12","H13","H14","H15","H16","H17","H18","H19","H20","H21","H22","H23",]
 
-        remove(Archivo)
+        remove("data_lake/raw/" + Archivo)
         Data.to_csv("data_lake/raw/" + NombreArchivo, sep=",", decimal=".", encoding="utf-8", index=False,)
             
     TodosArchivosCSV = []
