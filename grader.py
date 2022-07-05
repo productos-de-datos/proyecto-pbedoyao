2aae60b32808ab6c0ac0cb66502ca3439efc7a42
pbedoyao <101910326+pbedoyao@users.noreply.github.com>
2022-07-04 23:17:19 -0500

    ok

--- grader.py
+++ grader.py
@@ -142,7 +142,7 @@ def test_11():
 def test_12():
     """Pronosticos"""
     os.system("make make_forecasts")
-    assert os.path.isfile("data_lake/business/forecasts/precios-diarios.csv") is True
+    assert os.path.isfile("data_lake/business/forecasts-precios-diarios.csv") is True
 
 
 test = {
