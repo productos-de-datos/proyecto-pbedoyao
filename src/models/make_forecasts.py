577ab07fe35411893836805bb45d7edd6b5fca1f
pbedoyao <101910326+pbedoyao@users.noreply.github.com>
2022-07-04 19:47:15 -0500

    ok

--- src/models/make_forecasts.py
+++ src/models/make_forecasts.py
@@ -12,32 +12,33 @@ def make_forecasts():
 
     """
     import pandas as pd
-    from Comun import DatosTrainTest
-    from Comun import CargarDatos
-    from Comun import CargarMejorModelo
+    from train_daily_model import load_best_estimator
+    from train_daily_model import make_train_test_split
+    from train_daily_model import load_data
 
     #Cargamos los datos necesarios para calcular el pronostico 
 
-    x, y = CargarDatos()
-    Estimator = CargarMejorModelo()
-    x_train, x_test, y_train, y_test = DatosTrainTest(x, y)
-    y_pred = Estimator.predict(x)
+    x, y = load_data()
+    estimator = load_best_estimator()
+    x_train, x_test, y_train, y_test = make_train_test_split(x, y)
+    y_pred = estimator.predict(x)
 
     #Cargamos el archivo de precios diarios
-    path_file = './data_lake/business/precios-diarios.csv'
+    path_file = r'data_lake/business/precios-diarios.csv'
 
     #Leemos el dataframe y le adicionamos la columna con los pronosticos
     datos = pd.read_csv(path_file, index_col=None, sep=',', header=0)
     datos['pronostico'] = y_pred
-    datos.columns = ['Fecha', 'Precio promedio real de la electricidad', 'Pronóstico del precio promedio real']
+    datos.columns = ['fecha', 'precio promedio real de la electricidad', 'pronóstico del precio promedio real']
     
-    datos.to_csv('./data_lake/business/forecasts/precios-diarios.csv', index=None)
+    #Guardamos el archivo
+    datos.to_csv('data_lake/business/forecasts/precios-diarios.csv', index=None)
+
 
     #raise NotImplementedError("Implementar esta función")
-    
-    if __name__ == "__main__":
-        import doctest
 
-        doctest.testmod()
-        
-        make_forecasts()
+if __name__ == "__main__":
+    import doctest
+
+    doctest.testmod()
+    make_forecasts()
